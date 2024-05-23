1d20ccaa84edbf33a59e230b97865421caa2b142
Agus Di Tomaso <133171117+DiTomasoUCC@users.noreply.github.com>
2024-05-23 18:23:16 -0300

    Normalizacion de carpetas y archivos necesarios para comenzar a codear

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,1 @@
 package main
-
-import (
-	"log"
-
-	"github.com/DiTomasoUCC/arqui-software-I-tp-final-backend/cmd/api"
-)
-
-func main() {
-	server := api.NewAPIserver(":8080", nil)
-	if err := server.Run(); err != nil {
-		log.Fatal(err)
-	}
-}
