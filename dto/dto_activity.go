c1ea88bc0ddae4c4fc4bc72e0da3334cadc5c33a
MatiasCarbel <matiasjcarbel@gmail.com>
2024-05-27 16:44:16 -0300

    commit

--- dto/dto_activity.go
+++ dto/dto_activity.go
@@ -1,1 +1,9 @@
 package dto
+
+type ActivityDto struct {
+	UserId      int    `json:"user_id"`
+	CourseId    int    `json:"course_id"`
+	Description string `json:"description"`
+}
+
+type ActivitiesDto []ActivityDto
--- dto/dto_review.go
+++ dto/dto_review.go
@@ -1,1 +1,9 @@
 package dto
+
+type ReviewDto struct {
+	CourseId int    `json:"course_id"`
+	Comment  string `json:"comment"`
+	Stars    int    `json:"stars"`
+}
+
+type ReviewsDto []ReviewDto
