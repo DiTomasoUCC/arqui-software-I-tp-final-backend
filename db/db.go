c1d96c524e1b9ac52da871f4ba2011eb3113e100
MatiasCarbel <matiasjcarbel@gmail.com>
2024-06-04 13:55:19 -0300

    database connection

--- db/db.go
+++ db/db.go
@@ -24,8 +24,6 @@ func ConnectDatabase() error {
 
 	dsn := os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASSWORD") + "@tcp(" + os.Getenv("DB_SERVER") + ":" + os.Getenv("DB_PORT") + ")/" + os.Getenv("DB_NAME") + "?charset=utf8mb4&parseTime=True&loc=Local"
 
-	fmt.Println(dsn)
-
 	var err error
 	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
